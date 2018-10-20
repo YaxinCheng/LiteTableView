4b717496490138f759af7ffe073f6a8f3931c2e1
Yaxin Cheng <yaxin.cheng@dal.ca>
2018-10-20 16:00:48 -0400

    Initial Commit

---
+++ LiteTableView/LiteTableView.h
@@ -0,0 +1,19 @@
+//
+//  LiteTableView.h
+//  LiteTableView
+//
+//  Created by Yaxin Cheng on 2018-10-20.
+//  Copyright © 2018 Yaxin Cheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LiteTableView.
+FOUNDATION_EXPORT double LiteTableViewVersionNumber;
+
+//! Project version string for LiteTableView.
+FOUNDATION_EXPORT const unsigned char LiteTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiteTableView/PublicHeader.h>
+
+
